f13682ad2fe2cac60d255d47a0a123f8a522a081
Yulisa Velasquez <yulisap_2009@hotmail.com>
2023-01-26 22:21:46 -0500

    identificacion de transacciones BANSUR

--- conciliaciones.sql
+++ conciliaciones.sql
@@ -22,3 +22,22 @@ CREATE TABLE BANSUR (
     id_adquiriente VARCHAR,
     fecha_recepcion DATE
 );
+
+/**
+ Tiendo encuenta lo descrito por el enunciado:
+  "Una transacción regular se evidencia en la base de datos como un PAGO; se debe tener
+   en cuenta que un mismo ID puede también tomar estado de Cancelación, Chargeback u Otros casos."
+ Para esto creamos un id que nos permita identificar cada transacción en cada base da
+ datos (CLAP Y BANSUR) ya que en los datasets no viene dicho id, para lograrlo podemos usar el
+ número de la tarjeta, el código de autorización, el monto y el id del adquiriente como criterios
+ de unicidad, esto permitirá identificar cada transacción agrupando por estos campos.
+*/
+WITH transacciones AS (
+    SELECT tarjeta, codigo_autorizacion, abs(monto) AS monto_abs, id_adquiriente
+    FROM bansur
+    GROUP BY tarjeta, codigo_autorizacion, abs(monto), id_adquiriente
+    ORDER BY tarjeta
+)
+SELECT row_number() over (), *
+FROM transacciones
+;
